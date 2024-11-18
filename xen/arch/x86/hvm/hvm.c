38281266dbb367f9a741fde6016f7b3a7d255364
andSmv <andrei.semenov@vates.tech>
2024-11-18 17:44:28 +0100

    Added a check for SEV guest to not use clear guest buffer related functions as guest's linear addresses are used by the caller.
    Signed-off-by: andSmv <andrei.semenov@vates.tech>

--- xen/arch/x86/hvm/hvm.c
+++ xen/arch/x86/hvm/hvm.c
@@ -3499,6 +3499,9 @@ unsigned int clear_user_hvm(void *to, unsigned int len)
 {
     int rc;
 
+    if ( is_sev_domain(current->domain) )
+        return HVMTRANS_unhandleable;
+
     if ( current->hcall_compat && is_compat_arg_xlat_range(to, len) )
     {
         memset(to, 0x00, len);
