049ebdef9e8c8c6e6e301f7944599bffd8e656e4
andSmv <andrei.semenov@vates.tech>
2024-12-16 17:28:25 +0100

    Added sev_hox_demo_op hypercall to HyperOpenX project demonstrator
    This hypercall only use is to show that a guest memory protected by SEV
    technology isn't accessible from XEN hypervisor.
    
    Signed-off-by: andSmv <andrei.semenov@vates.tech>

--- xen/common/coco.c
+++ xen/common/coco.c
@@ -5,6 +5,8 @@
 #include <xen/types.h>
 #include <xen/errno.h>
 #include <xen/domain.h>
+#include <xen/domain_page.h>
+#include <asm/p2m.h>
 
 long
 do_dom_coco_op(unsigned int cmd, domid_t domid, uint64_t arg1, uint64_t arg2)
@@ -14,4 +16,53 @@ do_dom_coco_op(unsigned int cmd, domid_t domid, uint64_t arg1, uint64_t arg2)
 #else
     return -ENOSYS;
 #endif
+}
+
+long
+do_sev_hox_demo_op(uint64_t addr, uint64_t size)
+{
+    mfn_t mfn;
+    gfn_t gfn;
+    struct page_info *page;
+    p2m_type_t p2mt;
+
+    void* va;
+    uint8_t* start;
+
+    uint8_t offset = addr & ~ PAGE_MASK;
+
+    if ( (offset + size) > PAGE_SIZE )
+    {
+	printk("%s: the secret crosses page boundary\n", __FUNCTION__);
+	return -EINVAL;
+    }
+
+    gfn = gaddr_to_gfn(addr);
+
+    page = get_page_from_gfn(current->domain, gfn_x(gfn), &p2mt, P2M_UNSHARE);
+    if (!page)
+    {
+	printk("%s: can't get page for 0x%lx gfn\n", __FUNCTION__, gfn);
+    }
+
+    mfn = page_to_mfn(page);
+
+    va = map_domain_page(mfn);
+    if ( !va )
+    {
+	printk("%s: can't map domain address\n", __FUNCTION__);
+    }
+
+    printk("ACCESS GUEST SECRET\n");
+
+    for ( start = va + offset; size; start++, size-- )
+    {
+	printk("Ox%x ", *start);
+    }
+
+    printk("\nDONE GUEST SECRET\n");
+
+    unmap_domain_page(va);
+
+    return 0;
 }
